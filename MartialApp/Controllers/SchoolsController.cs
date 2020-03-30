b1345952d8ca3049e72b6c172c9b03abf614cd2a
Doug bjj <costadelsolbjj@outlook.com>
2020-03-30 02:35:56 +0200

    Added Authorize librarie

--- MartialApp/Controllers/SchoolsController.cs
+++ MartialApp/Controllers/SchoolsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MartialApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MartialApp.Controllers
 {
